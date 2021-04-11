8588ddc58aa5ddc11414b4b572c0de1a2aec04e5
antonioolvera1995 <antonioolvera1995@gmail.com>
2021-04-11 14:13:53 +0200

    Auto version finish

--- version.bash
+++ version.bash
@@ -1,51 +1,45 @@
 #!/bin/sh
 
-#Get the highest tag number
 VERSION=`git describe --abbrev=0 --tags`
-VERSION=${VERSION:-'0.0.0'}
+VERSION=${VERSION:-'0.9.9'}
+if [ $VERSION == "0.9.9" ];
+then
+CURRENT_VERSION="1.0.0"
+else
+CURRENT_VERSION=$VERSION
+fi
 
-#Get number parts
+
+
+
 MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
 MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
 PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
 
-#Increase version
-PATCH=$((PATCH+1))
+if [ $PATCH -lt 9 ];
+then
+PATCH=$(($PATCH + 1))
+else
+PATCH=0;
+if [ $MINOR -lt 9 ];
+then
+MINOR=$(($MINOR + 1))
+else
+MINOR=0;
+if [ $MAJOR -lt 20 ];
+then
+MAJOR=$(($MAJOR + 1))
+else
+MAJOR=0;
+fi
+fi
+fi
 
-#Get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
-#Create new tag
 NEW_TAG="$MAJOR.$MINOR.$PATCH"
+echo "Current version $CURRENT_VERSION"
 echo "Updating to $NEW_TAG"
 
-#Only tag if no tag already (would be better if the git describe command above could have a silent option)
-if [ -z "$NEEDS_TAG" ]; then
-    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-    git tag $NEW_TAG
-else
-    echo "Already a tag on this commit"
-fi
-
-#create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
-
-#get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
-
-#only tag if no tag already
-#to publish, need to be logged in to npm, and with clean working directory: `npm login; git stash`
-if [ -z "$NEEDS_TAG" ]; then
-  npm version $NEW_TAG
-  npm publish --access public
-  echo "Tagged with $NEW_TAG"
-  git push --tags
-  git push
-else
-  echo "Already a tag on this commit"
-fi
-
+git tag -a "$NEW_TAG" -m "version $NEW_TAG"
+git push origin "$NEW_TAG"
 exit 0
