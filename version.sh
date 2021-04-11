ade2bc5d51c17a74b9bbeca5d05438d69b341505
antonioolvera1995 <antonioolvera1995@gmail.com>
2021-04-11 17:31:44 +0200

    finish auto version

---
+++ version.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+git fetch --prune --unshallow
+
+VERSION=`git describe --abbrev=0 --tags`
+VERSION=${VERSION:-'0.9.9'}
+if [ $VERSION == "0.9.9" ];
+then
+CURRENT_VERSION="1.0.0"
+else
+CURRENT_VERSION=$VERSION
+fi
+
+
+MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+
+if [ $PATCH -lt 9 ];
+then
+PATCH=$(($PATCH + 1))
+else
+PATCH=0;
+if [ $MINOR -lt 9 ];
+then
+MINOR=$(($MINOR + 1))
+else
+MINOR=0;
+if [ $MAJOR -lt 20 ];
+then
+MAJOR=$(($MAJOR + 1))
+else
+MAJOR=0;
+fi
+fi
+fi
+
+
+NEW_TAG="$MAJOR.$MINOR.$PATCH"
+echo "Current version $CURRENT_VERSION"
+echo "Updating to $NEW_TAG"
+
+git tag -a "$NEW_TAG" -m "version $NEW_TAG"
+git push origin "$NEW_TAG"
+exit 0
